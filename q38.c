8a86ce64f65c0ce7d566700c7272d52b780cf618
Mehak Dhamija <MEHAK.22835@stu.upes.ac.in>
2026-03-16 20:54:49 +0530

    Create q38.c

---
+++ q38.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define MAX 100000
+void maxSlidingWindow(int nums[], int n, int k)
+{
+    int dq[MAX]; // stores indices
+    int front = 0, rear = -1;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(front <= rear && dq[front] <= i - k)
+            front++;
+        while(front <= rear && nums[dq[rear]] < nums[i])
+            rear--;
+        dq[++rear] = i;
+        if(i >= k - 1)
+            printf("%d ", nums[dq[front]]);
+    }
+}
+int main()
+{
+    int nums[] = {1,3,-1,-3,5,3,6,7};
+    int n = 8;
+    int k = 3;
+    maxSlidingWindow(nums, n, k);
+    return 0;
+}
