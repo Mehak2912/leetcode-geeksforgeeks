2b2e873489e0685ab6f85250de960d2812744e97
Mehak Dhamija <MEHAK.22835@stu.upes.ac.in>
2026-03-18 22:55:41 +0530

    Create q45.c

---
+++ q45.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+
+    return 1 + (left > right ? left : right);
+}
