75de5ccc6ddf48eb12f883091db0c94133215d85
Mehak Dhamija <MEHAK.22835@stu.upes.ac.in>
2026-03-19 23:27:50 +0530

    Create q47.c

---
+++ q47.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+    return (left > right ? left : right) + 1;
+}
+int main() {
+    struct TreeNode* root = newNode(3);
+    root->left = newNode(9);
+    root->right = newNode(20);
+    root->right->left = newNode(15);
+    root->right->right = newNode(7);
+    printf("Maximum Depth: %d\n", maxDepth(root));
+    return 0;
+}
